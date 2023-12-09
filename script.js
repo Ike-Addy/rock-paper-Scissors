57d3098c2905445a38d1b7146504a4bdef095cbd
Ike Addy <ikaddy05@gmail.com>
2023-12-09 09:32:29 -0500

    Created game utilizing alerts and prompts Next is to create game utilizing buttons

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-function playRound () {
-    const playerSelection = prompt('Rock, Paper, or Scissor?')
-    alert(`You chose ${playerSelection}`);
-    alert(`The computer chose ${computerSelection()}`);
+function playRound (player, comp) {
+    player = playerSelection();
+    comp = computerSelection();
 
-    if (playerSelection === computerSelection) alert('its a tie')
-    if (playerSelection === 'rock' && computerSelection === 'scissor') {
-        alert('You win!')
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper') {
-        alert('You win!')
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        alert('You win!')
+    if (player == comp) {
+        alert('It\'s a tie');
+    } else if (player == 'rock' && comp == 'scissor') {
+        alert('You win!');
+    } else if (player == 'scissor' && comp == 'paper') {
+        alert('You win!');
+    } else if (player == 'paper' && comp == 'rock') {
+        alert('You win!');
     } else {
-        alert('You lose! :(')
+        alert('You lose D:');
     }
 }
 
 function computerSelection () {
-    let aiChoice = Math.floor(Math.random() * 3);
-    if (aiChoice === 0) aiChoice = 'rock';
-    if (aiChoice === 1) aiChoice = 'paper';
-    if (aiChoice === 2) aiChoice = 'scissor';
+    let aiChoice = Math.floor(Math.random() * 3)
+    if (aiChoice === 0) {
+        aiChoice = 'rock';
+    } else if (aiChoice === 1) {
+        aiChoice = 'paper';
+    } else if (aiChoice === 2) {
+        aiChoice = 'scissor';
+    } else {
+        aiChoice = null;
+    }
+    alert(`The computer chose ${aiChoice}`)
     return aiChoice;
+}
+
+function playerSelection () {
+    let playerChoice = prompt('Rock, Paper, or Scissor?')
+    alert(`You chose ${playerChoice}`);
+    return playerChoice;
 }
 
 playRound();
 
-
-//const computerSelectionRock
